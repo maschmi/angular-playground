956c245f8621455682a54f5c53a12111591b88da
Martin Schmidt <inw@immernurwollen.de>
2020-06-27 10:30:59 +0200

    scaffold selection-list library

---
+++ projects/selection-list/src/lib/selection-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'inw-selection-list',
+  template: `
+    <p>
+      selection-list works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SelectionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
