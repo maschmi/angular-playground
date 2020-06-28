5d6d0761e223d9fa3ea83aac8d6901c23f910613
Martin Schmidt <inw@immernurwollen.de>
2020-06-28 16:39:47 +0200

    adds forgotten module

---
+++ projects/showcase/src/app/app-material.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule
+  ]
+})
+export class AppMaterialModule { }
