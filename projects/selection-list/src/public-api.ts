416c70ef0f743830860193eded847cc6abb1d1f7
Martin Schmidt <inw@immernurwollen.de>
2020-07-04 14:12:32 +0200

    removes service

--- projects/selection-list/src/public-api.ts
+++ projects/selection-list/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of selection-list
  */
 
-export * from './lib/selection-list.service';
 export * from './lib/selection-list.component';
 export * from './lib/selection-list.module';
 export {SelectionListConfig} from './lib/config/selection-list.config';
