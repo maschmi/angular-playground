0a5e5a6216c2660e681518cf60e56fe800d8d722
Martin Schmidt <inw@immernurwollen.de>
2020-11-08 19:19:12 +0100

    fixes test dependencies

--- projects/showcase/src/test/helper/mock-components/mock-selection-list.component.ts
+++ projects/showcase/src/test/helper/mock-components/mock-selection-list.component.ts
@@ -1,8 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   template: '',
   // tslint:disable-next-line:component-selector
   selector: 'inw-selection-list'
 })
-export class MockSelectionListComponent {}
+export class MockSelectionListComponent {
+  @Input() listData: any[];
+  @Input() selectedElement: any;
+  @Output() selectionChanged = new EventEmitter<any>();
+}
